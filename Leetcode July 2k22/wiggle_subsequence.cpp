77ad55f30b074f1c79ac9149350fdbda5c03c068
Manishkip <89375969+Manishkip@users.noreply.github.com>
2022-07-03 23:21:35 +0530

    Update wiggle_subsequence.cpp

--- Leetcode July 2k22/wiggle_subsequence.cpp
+++ Leetcode July 2k22/wiggle_subsequence.cpp
@@ -1,3 +1,5 @@
+//Solution 1
+
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
@@ -7,5 +9,25 @@ public:
             else if(nums[i] - nums[i-1] < 0) dec = inc +1;
         }
         return max(inc, dec);
+    }
+};
+
+// Solution 2 from Ayushi Sharma
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if(nums.size()< 2)
+            return nums.size();
+        int prev = nums[1] - nums[0];
+        int count = prev != 0 ? 2: 1;
+        
+        for(int i = 2; i < nums.size(); i++){
+           int  diff = nums[i] - nums[i-1];
+            if((diff< 0 && prev >= 0) || diff>0 && prev <= 0) {
+                count++;
+                prev = diff;
+            }
+        }
+        return count;
     }
 };
